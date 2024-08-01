c25e3e8d20f5887142b22f223e1599ec9381e91d
«Andrew» <gepatytich@gmail.com>
2024-08-01 11:57:26 +0700

    added lesson 02

--- src/test/java/ru/gb/seminars/seminar_01/AbstractTest.java
+++ src/test/java/ru/gb/seminars/seminar_01/AbstractTest.java
@@ -3,7 +3,7 @@ package ru.gb.seminars.seminar_01;
 import org.junit.jupiter.api.BeforeAll;
 
 /**
- * абстрактный тест для игры BlackJack
+ * Абстрактный тест для игры BlackJack
  */
 public abstract class AbstractTest {
 
